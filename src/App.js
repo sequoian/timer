bf961eb750296cf2529f1b473b223b5272465686
Zach Askins <zach303@msn.com>
2017-05-29 15:42:22 -0700

    basic structure and layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class TimerInputs extends Component {
+  render() {
+    const categories = ['hours', 'minutes', 'seconds'];
+    const inputs = categories.map((item) => {
+      return (
+        <div>
+          <label for={item}>{item}</label>
+          <input 
+            id={item}
+            type="number"
+            min="0"
+            max="99"
+          />
+        </div>
+      );
+    })
+
+    return (
+      <form className="timer-inputs">
+        {inputs}
+      </form>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Timer
+        <div className="timer">
+          <TimerInputs />
+        </div>
+        <div className="controls">
+          <button>Start</button>
+          <button>Clear</button>
+        </div>
       </div>
     );
   }
